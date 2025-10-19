8781da256a10b17ce380ab9d195fd533efb09959
desynkd <ravindupeeris180@gmail.com>
2025-10-19 10:28:44 +0530

    Thought post card design

--- lib/presentation/widgets/common/musicplayer_bar.dart
+++ lib/presentation/widgets/common/musicplayer_bar.dart
@@ -361,7 +361,7 @@ class _MusicPlayerBarState extends State<MusicPlayerBar> {
     }
   }
 
-  String _truncate(String text, [int maxLength = 30]) {
+  String _truncate(String text, [int maxLength = 25]) {
     if (text.length <= maxLength) return text;
     return text.substring(0, maxLength) + '...';
   }
